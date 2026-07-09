2f364ca11a3d05e43e1861675116c5bb896e3204
Nivedita Vishwanathan <vishwanathannivedita54@gmail.com>
2026-07-09 17:08:20 +0000

    Swap.java

---
+++ Basic code/Swap.java
@@ -0,0 +1,12 @@
+class Swap{
+    public static void main(String args[])
+    {
+    int a =5;
+    int b = 8;
+    System.out.println("Before swapping: a= "+a+" b= "+b);
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println("After swapping: a= "+a+" b= "+b);   
+}
+}
